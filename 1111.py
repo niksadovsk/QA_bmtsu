ea71672c95fe3c5b213ed5b533017f61f7579ba1
niksadovsk <138056770+niksadovsk@users.noreply.github.com>
2023-07-04 18:14:32 +0300

    upload code file

---
+++ 1111.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+URL = 'https://www.saucedemo.com/'
+LOGIN = 'standard_user'
+PASSWORD = 'secret_sauce'
+
+def get_driver():
+  
+  driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+  driver.implicitly_wait(10)
+  return driver
+
+def get_element(driver, id):
+  element = driver.find_element(By.ID, id)
+  return element
+
+def open_page(driver, url):
+  driver.get(url)
+
+def fill_form (driver, id, data):
+  element = get_element(driver, id)
+  element.send_keys(data)
+  
+def element_click (driver, id):
+  element = get_element(driver, id)
+  element.click()
+
+def login(driver, name, password):
+  fill_form(driver, 'user-name', name)
+  fill_form(driver, 'password', password)
+  element_click(driver, 'login-button')
+
+  
+driver = get_driver() 
+open_page(driver, URL)
+login(driver = driver, name = LOGIN, password = PASSWORD)
+driver.quit()
+
+
